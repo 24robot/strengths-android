f57798fc1914b43d5bb0ae1d01cff1a34e45b7e7
Anirudh S <anirudh24seven@gmail.com>
2014-03-12 19:31:02 +0530

    Modify app icon and rearrange ordering of input EditText above the Strengths Label

--- app/build/source/r/debug/com/barter/strengths/R.java
+++ app/build/source/r/debug/com/barter/strengths/R.java
@@ -15,13 +15,13 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
-        public static final int buttonAddStrength=0x7f070004;
+        public static final int buttonAddStrength=0x7f070002;
         public static final int checkStrengthItem=0x7f070006;
         public static final int listViewStrength=0x7f070005;
-        public static final int loadingProgressBar=0x7f070002;
+        public static final int loadingProgressBar=0x7f070004;
         public static final int menu_refresh=0x7f070007;
-        public static final int textNewStrength=0x7f070003;
-        public static final int textViewTitle=0x7f070001;
+        public static final int textNewStrength=0x7f070001;
+        public static final int textViewTitle=0x7f070003;
     }
     public static final class layout {
         public static final int activity_to_do=0x7f030000;
